a20b89c7b2bf38fafefb23d186c1713a85d60603
jaydeb-ds <das294761@gmail.com>
2025-11-17 21:22:44 +0530

    tailwind intall

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <h1 className="underline text-red-100 text-3xl font-serif bg-amber-400">hello</h1>
     </>
   );
 }
